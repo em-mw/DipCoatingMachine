3cc18b9f730986aa6f3a8d75479a37cbfce37b43
em-mw <01350311max247@gmail.com>
2026-08-20 11:45:10 -0500

    fixed windows pico path finding

--- ui_programmer/programmer/picotran.py
+++ ui_programmer/programmer/picotran.py
@@ -2,18 +2,45 @@ import psutil
 import platform
 import os
 import shutil
+import ctypes
+
+def get_volume_label(drive_letter):
+
+    drive = f"{drive_letter.strip().upper()[0]}:\\"
+    
+    volume_name_buffer = ctypes.create_unicode_buffer(1024)
+    
+    success = ctypes.windll.kernel32.GetVolumeInformationW(
+        ctypes.c_wchar_p(drive),
+        volume_name_buffer,
+        ctypes.sizeof(volume_name_buffer),
+        None, None, None, None, 0
+    )
+    
+    if success:
+        return volume_name_buffer.value
+    else:
+        raise ctypes.WinError()
 
 def find_pico_drive():
     for part in psutil.disk_partitions(all=False):
         try:
             if platform.system() == "Windows":
                 volume_label = os.path.basename(part.mountpoint.rstrip("\\/"))
+                try:
+                    print("H",get_volume_label(part.device))
+                    print("j",   part.mountpoint)
+                except:pass
+                try:
+                    if "RPI-RP2" in get_volume_label(part.mountpoint) or "CIRCUITPY" in get_volume_label(part.mountpoint):
+                        return part.device, part.mountpoint
+                except:pass
             else:
                 # On Linux, check mount path directly
                 volume_label = os.path.basename(part.mountpoint)
             
-            if "RPI-RP2" in volume_label or "CIRCUITPY" in volume_label:
-                return part.device, part.mountpoint
+                if "RPI-RP2" in volume_label or "CIRCUITPY" in volume_label:
+                    return part.device, part.mountpoint
         except Exception:
             continue
     return None, None
